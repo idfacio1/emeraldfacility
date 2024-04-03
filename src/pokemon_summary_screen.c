db7cf1f06d08e7b688d6bec9f17c03410de0b9ea
svcheats3 <161892789+idfacio1@users.noreply.github.com>
2024-04-03 11:16:59 -0500

    HMs can be forgotten

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -2318,12 +2318,7 @@ static void Task_HandleReplaceMoveInput(u8 taskId)
 
 static bool8 CanReplaceMove(void)
 {
-    if (sMonSummaryScreen->firstMoveIndex == MAX_MON_MOVES
-        || sMonSummaryScreen->newMove == MOVE_NONE
-        || IsMoveHM(sMonSummaryScreen->summary.moves[sMonSummaryScreen->firstMoveIndex]) != TRUE)
         return TRUE;
-    else
-        return FALSE;
 }
 
 static void ShowCantForgetHMsWindow(u8 taskId)
